5599398498301352c557a40c320c5f881b42d9a5
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2024-11-19 15:20:25 +0000

    attempt at fixing circular issue

--- rogues_den/routes.py
+++ rogues_den/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import current_user, login_required
-from rogues_den import app, db
+from rogues_den import db
 from rogues_den.models import Users, Character
 
 
